213b12521a58a6850e721dac941b4cc0c581955b
Erkz <Erkiparna7@gmail.com>
2023-09-18 15:48:58 +0300

    Harjutus 2

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
+var CharCounter = /** @class */ (function () {
+    function CharCounter(adder) {
+        this.adder = adder;
+    }
+    CharCounter.prototype.addWordCharacters = function (word) {
+        this.adder.add(word.length);
+    };
+    CharCounter.prototype.getCharacterCount = function () {
+        return this.adder.getSum();
+    };
+    // New method to calculate the sum of lengths of words in an array
+    CharCounter.prototype.addWordsCharacters = function (words) {
+        for (var _i = 0, words_1 = words; _i < words_1.length; _i++) {
+            var word = words_1[_i];
+            this.addWordCharacters(word);
+        }
+    };
+    return CharCounter;
+}());
 var SimpleAdder = /** @class */ (function () {
     function SimpleAdder() {
         this.sum = 0;
     }
     SimpleAdder.prototype.add = function (nr) { this.sum += nr; };
-    SimpleAdder.prototype.zero = function () { this.sum = 0; };
     SimpleAdder.prototype.getSum = function () {
         return this.sum;
     };
     return SimpleAdder;
 }());
 var adder1 = new SimpleAdder();
-adder1.add(3);
-adder1.add(5);
-adder1.add(29);
-adder1.add(50);
-adder1.zero();
-console.log(adder1.getSum());
+var counter1 = new CharCounter(adder1);
+var words = ["Juku", "tuli", "kooli", "täna"];
+counter1.addWordsCharacters(words);
+console.log(counter1.getCharacterCount());
--- index.ts
+++ index.ts
@@ -1,22 +1,39 @@
 interface Adder{
     add(nr: number):void;
-    zero():void;
     getSum():number;
+}
+
+class CharCounter {
+    constructor(protected adder: Adder) {}
+
+    addWordCharacters(word: string): void {
+        this.adder.add(word.length);
+    }
+
+    getCharacterCount(): number {
+        return this.adder.getSum();
+    }
+
+    // New method to calculate the sum of lengths of words in an array
+    addWordsCharacters(words: string[]): void {
+        for (const word of words) {
+            this.addWordCharacters(word);
+        }
+    }
 }
 
 class SimpleAdder implements Adder{
     protected sum:number=0;
     add(nr:number){this.sum+=nr;}
-    zero(){this.sum=0;}
     getSum(): number {
         return this.sum;
     }
 }
 
 let adder1:Adder=new SimpleAdder();
-adder1.add(3);
-adder1.add(5);
-adder1.add(29);
-adder1.add(50);
-adder1.zero();
-console.log(adder1.getSum());
+let counter1:CharCounter=new CharCounter(adder1);
+
+const words = ["Juku", "tuli", "kooli"];
+counter1.addWordsCharacters(words);
+
+console.log(counter1.getCharacterCount());
