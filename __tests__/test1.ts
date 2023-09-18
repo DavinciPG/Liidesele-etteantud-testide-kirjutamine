29de402f6c1f1ffe17f48bb4bca48bbd531765f8
Erkz <Erkiparna7@gmail.com>
2023-09-18 16:26:39 +0300

    Harjutus 7

--- __tests__/test1.ts
+++ __tests__/test1.ts
@@ -1,10 +1,29 @@
 import {Adder, SimpleAdder} from "../index";
 
-test('test1', () => {
-    let adder:Adder=new SimpleAdder();
+let adder: Adder;
+
+beforeEach(() => {
+    adder=new SimpleAdder();
+})
+
+test('start', () => {
     expect(adder.getSum()).toBe(0);
+});
+
+test('one value', () => {
     adder.add(3);
     expect(adder.getSum()).toBe(3);
+});
+
+test('two values', () => {
+    adder.add(3);
     adder.add(5);
     expect(adder.getSum()).toBe(8);
+});
+
+test('after zeroing functions correctly', () => {
+   adder.add(6);
+   adder.zero();
+   adder.add(6);
+   expect(adder.getSum()).toBe(6);
 });
--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 interface Adder{
     add(nr: number):void;
+    zero():void;
     getSum():number;
 }
 
 class SimpleAdder implements Adder{
     protected sum:number=0;
     add(nr:number){this.sum+=nr;}
+    zero(){this.sum=0;}
     getSum(): number {
         return this.sum;
     }
